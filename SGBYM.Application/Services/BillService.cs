558e12085034ff2850819875d67032e55df45036
Yosoyret <randyenyer1@gmail.com>
2025-07-03 20:07:39 -0400

    feat: integracion entre citas y bills en el mismo dto

---
+++ SGBYM.Application/Services/BillService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SGBYM.Application.DTOs.AdminitratorDTO;
+using SGBYM.Application.DTOs.BillsDTO;
+using SGBYM.Application.Interfaces;
+using SGBYM.Domain.Interfaces;
+using SGBYM.Domain.Models;
+using SGBYM.Infrastructure.Data;
+
+namespace SGBYM.Application.Services
+{
+    public class BillService 
+    {
+        private readonly AppDbContext _context;
+        public BillService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<string> GenerarSerial()
+        {
+            var fecha = DateTime.Today;
+            int totalFactura = await _context.bills
+                .CountAsync(f => f.fechaEmision.Date == fecha);
+
+            int nuevoNumero = totalFactura + 1;
+
+            string fechaParte = fecha.ToString("yyyyMMdd");
+            string numeroParte = nuevoNumero.ToString("DS");
+
+            return $"FACT-{fechaParte}-{numeroParte}";
+        }
+        
+    }
+}
---
+++ SGBYM.Infrastructure/Repositories/BillsRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SGBYM.Domain.Interfaces;
+using SGBYM.Domain.Models;
+using SGBYM.Infrastructure.Data;
+
+namespace SGBYM.Infrastructure.Repositories
+{
+    public class BillsRepository : IBillRepository
+    {
+        private readonly AppDbContext _context;
+        public BillsRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task createBills(Bills bills)
+        {
+            _context.bills.Add(bills);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Bills>> GetAllBill()
+        {
+            return await _context.bills.ToListAsync();
+        }
+
+        public async Task<Bills> getBySerial(string serial)
+        {
+            return await _context.bills.FindAsync(serial);
+
+
+        }
+
+
+    }
+}
